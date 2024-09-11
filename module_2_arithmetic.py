6e01087ed38b55ba5021cfc3b416f7e7b18c86f5
Damien Altenburg <daltenburg@rrc.ca>
2024-09-11 16:08:21 -0500

    Add script to demonstrate arithmetic operations.

---
+++ module_2_arithmetic.py
@@ -0,0 +1,22 @@
+print(1 + 1)
+print(10 % 5)
+
+age = 26
+
+print(age + 1)
+
+print(age)
+
+#age = age + 1
+age += 1
+
+# Java
+# age++;
+
+print(age)
+
+brothers_age = age - 9
+
+print(brothers_age)
+
+# String Concatenation
